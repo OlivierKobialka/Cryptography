d5782a0d89b5030e2ebde82ad28ae2469a5c3903
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-09-05 20:15:33 +0200

    a

--- ECC/eccChart.py
+++ ECC/eccChart.py
@@ -1,10 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-a = int(input("Select height: "))
-b = int(input("Select width: "))
 
-y, x = np.ogrid[-5:5:100j, -5:5:100j]
-plt.contour(x.ravel(), y.ravel(), pow(y, 2) - pow(x, 3) - x * a - b, [0])
-plt.grid()
-plt.show()
+def visualize():
+	a = int(input("Select height: "))
+	b = int(input("Select width: "))
+
+	y, x = np.ogrid[-5:5:100j, -5:5:100j]
+	plt.contour(x.ravel(), y.ravel(), pow(y, 2) - pow(x, 3) - x * a - b, [0])
+	plt.grid()
+	plt.show()
+
+
+# def point_multiplication():
+
+
+visualize()
